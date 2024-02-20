891101fd19df656c053e7731352df3212cbe351f
SovereigntyOfShadows <160644016+SovereigntyOfShadows@users.noreply.github.com>
2024-02-20 17:59:08 +0000

    e

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# discordbottemplate
-- run `pip install -r requirements.txt`
+# discordbottemplatepip install -r requirements.txt
+- run ``
 - create `./token` and put bot token in it (no newline at end)
 - run `bot.py`
--- bot.py
+++ bot.py
@@ -1,15 +1,36 @@
 from discord.ext.commands import Bot,Context
-from discord import Intents
+from discord import Intents, Message
 
-bot = Bot('>',intents=Intents(messages=True))
+bot = Bot('>',intents=Intents.all())
 
 
 # from here you can edit to make more commands
 
 
 @bot.command()
-async def ping(ctx:Context):
-    await ctx.send("pong")
+async def hi(ctx:Context):
+    await ctx.send("fats")
+
+@bot.event
+async def on_message(message):
+    if bot.user.mentioned_in(message):
+        await message.channel.send('FUCK OFF')
+
+@bot.event
+async def on_message(msg:Message):
+    if msg.author.id !=bot.user.id:
+        if msg.content.lower().startswith("im"):
+            await message.channel.send(f"hi {msg.content[{}:]}, i'm dad")
+        if msg.content.lower().startswith("i'm"):
+            await message.channel.send(f"hi {msg.content[{}:]}, i'm dad")
+        if msg.content.lower().startswith("i am"):
+            await message.channel.send(f"hi {msg.content[{}:]}, i'm dad")
+
+
+@bot.command()
+async def bye(ctx:Context):
+    await ctx.send("you're ugly")
+
 
 
 # do not touch after this
